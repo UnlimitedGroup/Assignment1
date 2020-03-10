9389b41867318dbab483bd702e3cbc91249734dd
jordanwoodroffe <jordanwoodroffe@gmail.com>
2020-03-11 04:03:25 +1100

    Ideas

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -2,9 +2,5 @@ package controller;
 
 public class MainController {
 	public static void main(String[] args) {
-		System.out.println("test1");
-		System.out.println("test2");
-		System.out.println("test3");
-		System.out.println("test32362");
 	}
 }
