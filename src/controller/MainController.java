d3d6d98afbcd90a05363b1513a73c86a1a809b82
jordanwoodroffe <jordanwoodroffe@gmail.com>
2020-03-11 02:10:10 +1100

    test

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -2,6 +2,7 @@ package controller;
 
 public class MainController {
 	public static void main(String[] args) {
-		System.out.println("te");
+		System.out.println("test1");
+		System.out.println("test2");
 	}
 }
