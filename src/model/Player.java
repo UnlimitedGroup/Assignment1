6b99f0f5e099d0ba2e67aa737695d87acab43389
Jordan <jordanwoodroffe@gmail.com>
2020-03-28 23:43:49 +1100

    worked on initialising board

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,20 @@
 package model;
+import java.util.ArrayList;
 
 public class Player {
-
+	private int id;
+	ArrayList<Integer>idCheck = new ArrayList<Integer>();
+	
+	public Player(int id) throws idException {
+		if (!idCheck.isEmpty()) {
+			for (Integer x: idCheck) {
+				if (id != x) {
+					this.id = id;
+					idCheck.add(this.id);
+				}else {
+					throw new idException();
+				}
+			}
+		}
+	}
 }
