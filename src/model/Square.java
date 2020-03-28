197b269dfe6e77035953437179429faeca9d64b7
Jordan <jordanwoodroffe@gmail.com>
2020-03-29 01:43:34 +1100

    general updates

--- src/model/Square.java
+++ src/model/Square.java
@@ -4,10 +4,12 @@ package model;
 public class Square {
 	private int row;
 	private int column;
+	private boolean selectStatus;
 
-	public Square (int row, int column) {
+	public Square (int row, int column, boolean selectStatus) {
 		this.row = row;
 		this.column = column;
+		this.selectStatus = selectStatus;
 	}
 	
 	public int getRow() {
@@ -17,8 +19,12 @@ public class Square {
 	public int getColumn() {
 		return this.column;
 	}
-	
-	
-	
-
+	public boolean getSelectStatus() {
+		return this.selectStatus;
+	}
+	public boolean setSelectStatus() {
+		/*NEEDS WORK needs so that only one tile can be selected at one time
+		 */
+		return true;
+	}
 }
